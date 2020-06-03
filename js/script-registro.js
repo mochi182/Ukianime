d78617f4b245549e2925262bb874f4b7183e51f1
erandree <ernesto_031@hotmail.com>
2020-06-03 11:27:39 -0500

    Actualización al estilo base
    y algunos ajustes

--- js/script-registro.js
+++ js/script-registro.js
@@ -1,6 +1,6 @@
 
-function openCity(evt, cityName) {
-  var i, tabcontent, tablinks;
+function pestana(evt, tipo) {
+  var i, tabcontent, tablinks, activar;
   tabcontent = document.getElementsByClassName("tabcontent");
   for (i = 0; i < tabcontent.length; i++) {
     tabcontent[i].style.display = "none";
@@ -9,7 +9,7 @@ function openCity(evt, cityName) {
   for (i = 0; i < tablinks.length; i++) {
     tablinks[i].className = tablinks[i].className.replace(" active", "");
   }
-  document.getElementById(cityName).style.display = "block";
+  document.getElementById(tipo).style.display = "block";
   evt.currentTarget.className += " active";
 }
 
